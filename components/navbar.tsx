1cc1d05399749de3f6d0b8e28f33e5b72982a239
johnnyzip <127410610+johnnyzip@users.noreply.github.com>
2025-03-27 07:25:15 +0000

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import { Github } from "lucide-react"
 
 export default function Navbar() {
   return (
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <Link rel="icon" type="image/x-icon" href="/favicon.ico"></Link>
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <Link href="/" className="mr-6 flex items-center space-x-3">
